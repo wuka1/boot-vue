820e2a7e28ae97ab6554c247ebd137393777c9ce
wuka1 <wka2010@163.com>
2024-07-18 09:56:01 +0800

    修订包名

--- boot/src/main/java/com/example/boot/controller/PathVariableController.java
+++ boot/src/main/java/com/example/boot/controller/PathVariableController.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.boot.demos.web;
+package com.example.boot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
--- boot/src/main/java/com/example/boot/controller/User.java
+++ boot/src/main/java/com/example/boot/controller/User.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.boot.demos.web;
+package com.example.boot.controller;
 
 /**
  * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
