7be30c3705c59786e7e8e39fb13ef580bd37a2a3
wuka1 <wka2010@163.com>
2024-07-18 09:28:50 +0800

    初始化

---
+++ boot/src/main/java/com/example/boot/BootApplication.java
@@ -0,0 +1,16 @@
+package com.example.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuka
+ */
+@SpringBootApplication
+public class BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+
+}
